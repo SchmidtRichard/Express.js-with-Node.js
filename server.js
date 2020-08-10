7dff800e38a5fb5a5f4463127bc64720efbbd89f
SchmidtRichard <ri.schmidta@gmail.com>
2020-08-10 11:59:48 +0100

    Understanding and Working with Routes

--- server.js
+++ server.js
@@ -15,6 +15,18 @@ app.get("/", function(request, response) {
   response.send("<h1>Hello there panda safadao</h1>");
 });
 
+app.get("/contact", function(request, response) {
+  response.send("Contact me at: richard@fuinha.com");
+});
+
+app.get("/about", function(request, response) {
+  response.send("I am Fuinha");
+});
+
+app.get("/hobbies", function(request, response) {
+  response.send("<ul><li>skating</li><li>programming</li><li>cooking</li></ul>")
+});
+
 //Method listen to listen to a specific port for a HTTP request that gets sent to the server
 //Call back function to the listen method to see when the port is setup and the server is running
 app.listen(3000, function() {
